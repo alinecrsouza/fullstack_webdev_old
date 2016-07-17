72d1fe7e462fe44b0a2a4d262d685147865c8fc8
Aline Rodrigues <acrsouza@id.uff.br>
2016-07-17 16:56:52 -0300

    Exercise 3 Week 2 -> Form Validation

--- Angular-js/conFusion/app/scripts/app.js
+++ Angular-js/conFusion/app/scripts/app.js
@@ -1,71 +1,109 @@
 'use strict';
 //attach the module to the controller
-angular.module('confusionApp', []).controller('menuController', function() {
-  this.tab = 1;
-  this.filtText = '';
-  
-  var dishes = [
-    {
-      name:'Uthapizza',
-      image: 'images/uthapizza.png',
-      category: 'mains',
-      label:'Hot',
-      price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: ''
-    },
-    
-    {
-      name:'Zucchipakoda',
-      image: 'images/zucchipakoda.png',
-      category: 'appetizer',
-      label:'',
-      price:'1.99',
-      description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-      comment: ''
-    },
-    
-    {
-      name:'Vadonut',
-      image: 'images/vadonut.png',
-      category: 'appetizer',
-      label:'New',
-      price:'1.99',
-      description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-      comment: ''
-    },
-    
-    {
-      name:'ElaiCheese Cake',
-      image: 'images/elaicheesecake.png',
-      category: 'dessert',
-      label:'',
-      price:'2.99',
-      description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-      comment: ''
-    }
-  ]; 
-  
-  this.dishes = dishes;
+angular.module('confusionApp', []).controller('MenuController', ['$scope', function ($scope) {
+        $scope.tab = 1;
+        $scope.filtText = '';
+        $scope.showDetails = false;
 
-  this.select = function(setTab) {
-    this.tab = setTab;
-    
-    if (setTab === 2) {
-      this.filtText = "appetizer";
-    } 
-    else if (setTab === 3) {
-      this.filtText = "mains";
-    }
-    else if (setTab === 4) {
-      this.filtText = "dessert";
-    }
-    else {
-      this.filtText = "";
-    }
-  };
-  
-  this.isSelected = function (checkTab) {
-    return (this.tab === checkTab);
-  };
-});
+        var dishes = [
+            {
+                name: 'Uthapizza',
+                image: 'images/uthapizza.png',
+                category: 'mains',
+                label: 'Hot',
+                price: '4.99',
+                description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                comment: ''
+            },
+            {
+                name: 'Zucchipakoda',
+                image: 'images/zucchipakoda.png',
+                category: 'appetizer',
+                label: '',
+                price: '1.99',
+                description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+                comment: ''
+            },
+            {
+                name: 'Vadonut',
+                image: 'images/vadonut.png',
+                category: 'appetizer',
+                label: 'New',
+                price: '1.99',
+                description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
+                comment: ''
+            },
+            {
+                name: 'ElaiCheese Cake',
+                image: 'images/elaicheesecake.png',
+                category: 'dessert',
+                label: '',
+                price: '2.99',
+                description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                comment: ''
+            }
+        ];
+
+        $scope.dishes = dishes;
+
+        $scope.select = function (setTab) {
+            $scope.tab = setTab;
+
+            if (setTab === 2) {
+                $scope.filtText = "appetizer";
+            }
+            else if (setTab === 3) {
+                $scope.filtText = "mains";
+            }
+            else if (setTab === 4) {
+                $scope.filtText = "dessert";
+            }
+            else {
+                $scope.filtText = "";
+            }
+        };
+
+        $scope.isSelected = function (checkTab) {
+            return ($scope.tab === checkTab);
+        };
+
+        //code for the ngShow Directive
+        $scope.toggleDetails = function () {
+            $scope.showDetails = !$scope.showDetails;
+        };
+    }])
+        .controller('ContactController', ['$scope', function ($scope) {
+
+                $scope.feedback = {mychannel: "", firstName: "", lastName: "",
+                    agree: false, email: ""};
+            }])
+
+        .controller('FeedbackController', ['$scope', function ($scope) {
+
+            }])
+
+        .controller('ContactController', ['$scope', function ($scope) {
+                $scope.feedback = {mychannel: "", firstName: "", lastName: "", agree: false, email: ""};
+                var channels = [{value: "tel", label: "Tel."}, {value: "Email", label: "Email"}];
+                $scope.channels = channels;
+                $scope.invalidChannelSelection = false;
+            }])
+        .controller('FeedbackController', ['$scope', function ($scope) {
+                $scope.sendFeedback = function () {
+                    console.log($scope.feedback);
+                    if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+                        $scope.invalidChannelSelection = true;
+                        console.log('incorrect');
+                    }
+                    else {
+                        $scope.invalidChannelSelection = false;
+                        $scope.feedback = {mychannel: "", firstName: "", lastName: "",
+                            agree: false, email: ""};
+                        $scope.feedback.mychannel = "";
+
+                        $scope.feedbackForm.$setPristine();
+                        console.log($scope.feedback);
+                    }
+                };
+            }])
+        ;
